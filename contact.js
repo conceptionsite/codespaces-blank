c0474936890aecc858d0c2538a2f0e2c0ade4303
conceptionsite <120242816+conceptionsite@users.noreply.github.com>
2022-12-10 16:34:26 +0000

    first commit

---
+++ contact.js
@@ -0,0 +1,20 @@
+function loadLeafletMap(){
+        const map = L.map('map').setView([48.8269527, 2.3648285], 13);
+        const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        const marker = L.marker([48.8269527, 2.3648285]).addTo(map)
+    }
+
+
+    function onMapClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent(`You clicked the map at ${e.latlng.toString()}`)
+            .openOn(map);
+    }
+
+    map.on('click', onMapClick);
+
